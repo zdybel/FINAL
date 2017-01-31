f2ae459894df73cabe3120653d6a3601c38c1e4b
Brynne Zdybel <bazdybel@gmail.com>
2017-01-30 22:26:45 -0500

    i dont know

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
 window.onload = function() {
+
+	(function() {
+  	var supportsPassive = eventListenerOptionsSupported();  
+
+  	if (supportsPassive) {
+    	var addEvent = EventTarget.prototype.addEventListener;
+    	overwriteAddEvent(addEvent);
+  	}
+
+  	function overwriteAddEvent(superMethod) {
+    var defaultOptions = {
+      passive: true,
+      capture: false
+    };
+
+    EventTarget.prototype.addEventListener = function(type, listener, options) {
+      var usesListenerOptions = typeof options === 'object';
+      var useCapture = usesListenerOptions ? options.capture : options;
+
+      options = usesListenerOptions ? options : {};
+      options.passive = options.passive !== undefined ? options.passive : defaultOptions.passive;
+      options.capture = useCapture !== undefined ? useCapture : defaultOptions.capture;
+      
+      superMethod.call(this, type, listener, options);
+    };
+  }
+
+  function eventListenerOptionsSupported() {
+    var sopported = false;
+    try {
+      var opts = Object.defineProperty({}, 'passive', {
+        get: function() {
+          sopported = true;
+        }
+      });
+      window.addEventListener("test", null, opts);
+    } catch (e) {}
+
+    return sopported;
+  }
+})();
  
  	//object constructor for jobs. 
 	function Job(id, name, word, titleMarginTop){
@@ -18,6 +59,7 @@ window.onload = function() {
 	var fifthHeight = viewportHeight * .2;
 	var fifthHeightNeg = viewportHeight * -.2;
 	var thirdWidth = viewportWidth * (1/3);
+	var twoThirdWidth = viewportWidth * (2/3);
 
 	//set variables for DOM elements for readability. 
 	var bodyContainer = document.getElementsByClassName("body-container")[0];
@@ -58,6 +100,8 @@ window.onload = function() {
 
 	//add event listener so when window changes, viewportWidth variable also changes.
 	window.addEventListener("resize", changeViewportWidth);
+
+	window.addEventListener("mousewheel", sideScroll);
 
 	//change viewportWidth variable to current viewport width and set elements widths accordingly.
 	function changeViewportWidth(){
@@ -144,10 +188,10 @@ window.onload = function() {
 	}
 
 	//this function takes the mousewheel event, sends it through the above section (which sends it through normalize_mousewheel), and then tells the body_container div to move right if delta is positive(scroll up) and left if delta is negative(scroll down).
-	function sideScroll(){
-		var bodyContainer = document.getElementsByClassName("body-container")[0];
-		if ('onmousewheel' in bodyContainer) {
-		    bodyContainer.onmousewheel = function(e) {
+	function sideScroll(e){
+		// var bodyContainer = document.getElementsByClassName("body-container")[0];
+		// if ('onmousewheel' in bodyContainer) {
+		//     bodyContainer.onmousewheel = function(e) {
 		        e = e || window.event;
 		        listener(e);
 		        if (e.delta > 0){      
@@ -158,10 +202,10 @@ window.onload = function() {
 		     		window.scrollBy(25, 0);
 		     		scrollForward();
 				}
-			}
-		} else {
-		    bodyContainer.addEventListener('DOMMouseScroll', listener);
-		};
+			// }
+		// } else {
+		//     bodyContainer.addEventListener('DOMMouseScroll', listener);
+		// };
 	};
 
 	function mobileScroll(){
@@ -171,52 +215,152 @@ window.onload = function() {
 		});
 	};
 	// var thismargin = 0;
-	function scrollForward(){
-		console.log("pppp");
-		for(var i=1; i<jobs.length; i++){
-			//set job and element
-			var currentJob = jobs[i]
-			element = document.getElementsByClassName('job-content-title')[i];
-    		//set widths that the element will move at
-    		var widths = viewportWidth * (i-1);
-    		console.log(widths);
-    		var widthsAndAThird = widths + thirdWidth;
-    		var currentWidths = (viewportWidth * i) -300;
-    		var currentWidthsAndAThird = currentWidths + thirdWidth;
-    		if(window.scrollX >= widthsAndAThird && window.scrollX <= widthsAndAThird){
-    			while(currentJob.titleMarginTop >= -200){
-    				element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
-    				currentJob.titleMarginTop -= 5;
-    				console.log("its happening tow  " + currentJob.titleMarginTop.toString())
-    			};
-    		}
-    		if(window.scrollX < currentWidths && window.scrollX < currentWidthsAndAThird){
-    			while(currentJob.titleMarginTop < fifthHeight){
-    				element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
-    				currentJob.titleMarginTop += 5;
-    				// console.log("one");
-    			};
-    		};
-		};
-	};
+	// function scrollForward(){
+	// 	console.log("pppp");
+// 		switch (new Date().getDay()) {
+//     case 0:
+//         day = "Sunday";
+//         break;
+//     case 1:
+//         day = "Monday";
+//         break;
+//     case 2:
+//         day = "Tuesday";
+//         break;
+//     case 3:
+//         day = "Wednesday";
+//         break;
+//     case 4:
+//         day = "Thursday";
+//         break;
+//     case 5:
+//         day = "Friday";
+//         break;
+//     case  6:
+//         day = "Saturday";
+// }
+	// 	for(var i=1; i<jobs.length; i++){
+	// 		//set job and element
+	// 		var currentJob = jobs[i]
+	// 		element = document.getElementsByClassName('job-content-title')[i];
+ //    		//set widths that the element will move at
+ //    		var widths = viewportWidth * (i-1);
+ //    		console.log(widths);
+ //    		var widthsAndAThird = widths + thirdWidth;
+ //    		var currentWidths = (viewportWidth * i) -300;
+ //    		var currentWidthsAndAThird = currentWidths + thirdWidth;
+ //    		if(window.scrollX >= widthsAndAThird && window.scrollX <= widthsAndAThird){
+ //    			while(currentJob.titleMarginTop >= -200){
+ //    				element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+ //    				currentJob.titleMarginTop -= 5;
+ //    				console.log("its happening tow  " + currentJob.titleMarginTop.toString())
+ //    			};
+ //    		}
+ //    		if(window.scrollX < currentWidths && window.scrollX < currentWidthsAndAThird){
+ //    			while(currentJob.titleMarginTop < fifthHeight){
+ //    				element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+ //    				currentJob.titleMarginTop += 5;
+ //    				// console.log("one");
+ //    			};
+ //    		};
+	// 	};
+	// };
 
 	
 	function scrollBackward(){
-		// for(var i=0; i<jobs.length; i++){
-		// 	element = document.getElementsByClassName('job-content-title')[i];
-  //   		marginTopString = window.getComputedStyle(element)['marginTop'];
-  //   		marginTop = marginTopString.replace("px", "");
-  //   		marginTopInt = parseInt(marginTop);
-  //   		var margin = marginTopInt - Math.abs(thismargin);
-  //   		var widths = (viewportWidth * (i-1)) + 300;
-  //   		var widthsAndAThird = widths + thirdWidth;
-   //  		if(window.scrollX >= widthsAndAThird){
-   //  			if(marginTopInt >= -100){
-			// 		document.getElementsByClassName("job-content-title")[i].style.marginTop = margin.toString() + "px";
-			// 		thismargin = thismargin + .05;
-			// 	};
-			// };
-		// };
+	};
+	
+	function scrollForward(){
+		if(window.scrollX > thirdWidth && window.scrollX < twoThirdWidth){
+			var element = document.getElementsByClassName("job-content-title")[0];
+			var currentJob = jobs[0];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("0");
+	    	};
+		}
+		if(window.scrollX >twoThirdWidth && window.scrollX < (2*twoThirdWidth)){
+			var element = document.getElementsByClassName("job-content-title")[1];
+			var currentJob = jobs[1];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("1");
+	    	};	
+		}
+		if(window.scrollX >(2*twoThirdWidth) && window.scrollX < (3*twoThirdWidth)){
+			var element = document.getElementsByClassName("job-content-title")[2];
+			var currentJob = jobs[2];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("2");
+	    	};	
+		}
+		if(window.scrollX >(3*twoThirdWidth) && window.scrollX < (4*twoThirdWidth)){
+			var element = document.getElementsByClassName("job-content-title")[3];
+			var currentJob = jobs[3];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("3");
+	    	};	
+		}
+		if(window.scrollX >(4*twoThirdWidth) && window.scrollX < (5*twoThirdWidth)){
+			var element = document.getElementsByClassName("job-content-title")[4];
+			var currentJob = jobs[4];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("4");
+	    	};	
+		}
+		if(window.scrollX >(5*twoThirdWidth) && window.scrollX < (6*twoThirdWidth)){
+			var element = document.getElementsByClassName("job-content-title")[5];
+			var currentJob = jobs[5];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("5");
+	    	};	
+		}
+		if(window.scrollX >(6*twoThirdWidth) && window.scrollX < (7*twoThirdWidth)){
+			var element = document.getElementsByClassName("job-content-title")[6];
+			var currentJob = jobs[6];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("6");
+	    	};	
+		}
+		if(window.scrollX >(7*twoThirdWidth) && window.scrollX < (8*twoThirdWidth)){
+			var element = document.getElementsByClassName("job-content-title")[7];
+			var currentJob = jobs[7];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("7");
+	    	};	
+		}
+		if(window.scrollX >(8*twoThirdWidth) && window.scrollX < (9*twoThirdWidth)){
+			var element = document.getElementsByClassName("job-content-title")[8];
+			var currentJob = jobs[8];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("8");
+	    	};	
+		}
+		if(window.scrollX >(9*twoThirdWidth) && window.scrollX < (10*twoThirdWidth)){
+			var element = document.getElementsByClassName("job-content-title")[9];
+			var currentJob = jobs[9];
+			while(currentJob.titleMarginTop < fifthHeight){
+	    		element.style.marginTop = currentJob.titleMarginTop.toString() + "px";
+	    		currentJob.titleMarginTop += .5;
+	    		console.log("9");
+	    	};	
+		}
 	};
 };
 
